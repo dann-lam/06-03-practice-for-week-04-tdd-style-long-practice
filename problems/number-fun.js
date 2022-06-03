966f38d5783cef2484f358928ee39a9b8712c046
mathi <mathieu.t.evans@gmail.com>
2022-06-03 11:19:12 -0700

    numberhell

--- problems/number-fun.js
+++ problems/number-fun.js
@@ -1,10 +1,25 @@
 function returnsThree() {
-  // Your code here
+  return 3
 }
 
 function reciprocal(n) {
-  // Your code here
+  // if (n !== 0) {
+  //   return 1 / n
+  // } else {
+  //   throw Error("can't do that bub")
+  // } ALMOST WORKED
+
+  // Worked
+  if (n >= 1 && n <= 1000000) {
+    return 1 / n
+  } else if (n < 1) {
+    throw Error("negative energy")
+  } else if (n > 1000000) {
+    throw Error("his power levels are over 9000")
+  }
 }
+
+// console.log(reciprocal(0)) breaks mocha
 
 module.exports = {
   returnsThree,
--- test/number-fun-spec.js
+++ test/number-fun-spec.js
@@ -1,1 +1,25 @@
-// Your code here
+const chai = require('chai')
+const expect = chai.expect
+const { returnsThree, reciprocal } = require('../problems/number-fun')
+
+describe("returnsThree()", () => {
+    it("should return three", () => {
+        const example = returnsThree()
+        expect(example).to.eql(3)
+    })
+})
+
+describe("reciprocal(n)", () => {
+    it("should return the reciprocal", () => {
+        const example1 = reciprocal(2)
+        expect(example1).to.eql(1 / 2)
+    })
+    it("should throw an error", () => {
+        const rec2 = 0;
+        const rec3 = 10000000;
+        const rec4 = -1;
+        expect(() => reciprocal(rec2)).to.throw(Error);
+        expect(() => reciprocal(rec3)).to.throw(Error);
+        expect(() => reciprocal(rec4)).to.throw(Error);
+    })
+})
